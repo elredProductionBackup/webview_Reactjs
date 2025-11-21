3f8619b6619794396fb8a337c4b5631e5f2cbbf7
MeezanOne <meezan@elred.io>
2025-11-21 10:51:27 +0530

    content changes

--- src/TashafsModule/Profile/components/InfoData/InfoData.jsx
+++ src/TashafsModule/Profile/components/InfoData/InfoData.jsx
@@ -2,15 +2,30 @@ import './infodata.scss'
 
 const InfoData = ({rgba,data}) => {
 
-    const startsWithVowel = (word) => /^[aeiou]/i.test(word);
+const startsWithVowel = (word) => /^[aeiou]/i.test(word);
+const profession = data?.profession || "";
+const article = startsWithVowel(profession) ? "an" : "a";
 
-  const article = startsWithVowel(data?.profession) ? "an" : "a";
+return (
+<div className='infodata' style={{ background: `${rgba}` }}>
+I am <span>{article} <span className='lowercase-span'>{profession}</span></span>.
 
-  return (
-    <div className='infodata' style={{ background: `${rgba}` }}>
-        I am <span>{article} <span className='titlecase-span'>{data?.profession}</span></span>.{data?.industry?.industryName && <> My industry is <span className='titlecase-span'>{data?.industry?.industryName}</span>.</>} {data?.organizationName && <>My company / business name is <span className='titlecase-span'>{data?.organizationName}</span></>}
-    </div>
-  )
+  {data?.industry?.industryName && (
+    <> My industry is <span className='lowercase-span'>{data?.industry?.industryName}</span>.</>
+  )}
+
+  {data?.organizationName && (
+    profession.toLowerCase() === "student"
+      ? (
+        <> My high school / college name is <span className='lowercase-span'>{data?.organizationName}</span>.</>
+      )
+      : (
+        <> My company / business name is <span className='titlecase-span'>{data?.organizationName}</span>.</>
+      )
+  )}
+</div>
+
+)
 }
 
 export default InfoData
