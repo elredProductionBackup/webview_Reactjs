c27c27884db95512dd02f6f56b13bba60aa4bc97
MeezanOne <meezan@elred.io>
2025-11-21 09:31:11 +0530

    minor change in info for article logic

--- src/TashafsModule/Profile/components/InfoData/InfoData.jsx
+++ src/TashafsModule/Profile/components/InfoData/InfoData.jsx
@@ -1,9 +1,14 @@
 import './infodata.scss'
 
 const InfoData = ({rgba,data}) => {
+
+    const startsWithVowel = (word) => /^[aeiou]/i.test(word);
+
+  const article = startsWithVowel(data?.profession) ? "an" : "a";
+
   return (
     <div className='infodata' style={{ background: `${rgba}` }}>
-        I am <span>a <span className='titlecase-span'>{data?.profession}</span></span>.{data?.industry?.industryName && <> My industry is <span className='titlecase-span'>{data?.industry?.industryName}</span></>}. My company / business name is <span className='titlecase-span'>{data?.organizationName}</span>
+        I am <span>{article} <span className='titlecase-span'>{data?.profession}</span></span>.{data?.industry?.industryName && <> My industry is <span className='titlecase-span'>{data?.industry?.industryName}</span></>}. My company / business name is <span className='titlecase-span'>{data?.organizationName}</span>
     </div>
   )
 }
