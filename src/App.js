71f42985d7cb0e8a167d3b687ce0e910066abd61
elredProductionBackup <productionbackup@elred.io>
2026-04-07 12:37:12 +0530

    new

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import { toastConfig } from "react-simple-toasts";
 
 export const GlobalData = createContext();
 
+console.log('latest: 7 Apr')
+
 function App() {
   axios.defaults.headers.common[process.env.REACT_APP_DEFAULT_HEADER]
     = Math.random() > 0.5 ? process.env.REACT_APP_HEADER_KEY_1 : process.env.REACT_APP_HEADER_KEY_2;
